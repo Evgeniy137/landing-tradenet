21835f6a22a72eeb03235458d02e2086ae695ade
Evgeniy <vavilon11111111@gmail.com>
2021-05-13 10:39:35 +0300

    settings form

--- form/js/scripts.js
+++ form/js/scripts.js
@@ -22,13 +22,4 @@
 }) */
 
 var input = document.querySelector("#phone");
-window.intlTelInput(input, {
-    initialCountry: "auto",
-    geoIpLookup: function (callback) {
-        $.get('https://ipinfo.io', function () { }, "jsonp").always(function (resp) {
-            var countryCode = (resp && resp.country) ? resp.country : "us";
-            callback(countryCode);
-        });
-    },
-    utilsScript: "../../build/js/utils.js?1613236686837" // just for formatting/placeholders etc
-});
+window.intlTelInputUtils(input);
